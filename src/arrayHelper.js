88fc710b139357dbcdffe7f68ff6aa864b684f76
Ray Bergholm <raybergholm@gmail.com>
2018-03-17 01:07:57 +0200

    add default export

--- src/arrayHelper.js
+++ src/arrayHelper.js
@@ -1,12 +1,18 @@
-export const intersection = (left, right) => comparator => 
-left.filter((leftElement) => comparator ? right.find((rightElement) => comparator(leftElement, rightElement)) : right.indexOf(leftElement) !== -1);
+export const intersection = (left, right) => comparator =>
+    left.filter((leftElement) => comparator ? right.find((rightElement) => comparator(leftElement, rightElement)) : right.indexOf(leftElement) !== -1);
 
-export const leftDifference = (left, right) => comparator => 
+export const leftDifference = (left, right) => comparator =>
     left.filter((leftElement) => comparator ? !right.find((rightElement) => comparator(leftElement, rightElement)) : right.indexOf(leftElement) === -1);
 
-export const rightDifference = (left, right) => comparator => 
+export const rightDifference = (left, right) => comparator =>
     right.filter((rightElement) => comparator ? !left.find((leftElement) => comparator(leftElement, rightElement)) : left.indexOf(rightElement) === -1);
 
-export const symmetricalDifference = (left, right) => comparator => 
-    leftDifference(left, right)(comparator)
-        .concat(rightDifference(left, right)(comparator));
+export const symmetricalDifference = (left, right) => comparator =>
+    leftDifference(left, right)(comparator).concat(rightDifference(left, right)(comparator));
+
+export default {
+    intersection,
+    leftDifference,
+    rightDifference,
+    symmetricalDifference
+};
